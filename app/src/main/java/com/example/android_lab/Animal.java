af992faedb1c44fde093c94394b406870a93b7d3
lapeace <piotr.lapis@gmail.com>
2019-05-16 13:13:45 +0200

    Edycja zwierzaków

--- app/src/main/java/com/example/android_lab/Animal.java
+++ app/src/main/java/com/example/android_lab/Animal.java
@@ -9,27 +9,45 @@ public class Animal implements Serializable {
     private float wielkosc;
     private String opis;
 
-    public Animal (){}
+    public Animal() {
+    }
 
-    public Animal(String gatunek, String kolor, float wielkosc, String opis){
+    public Animal(String gatunek, String kolor, float wielkosc, String opis) {
         super();
-        this.gatunek=gatunek;
+        this.gatunek = gatunek;
         this.kolor = kolor;
         this.wielkosc = wielkosc;
-        this.opis=opis;
+        this.opis = opis;
     }
 
     @Override
-    public String toString(){
-        return "Zwierze: [id=" + _id + ", gatunek=" + gatunek + ", kolor= " + kolor + ", wielkosc = " + wielkosc+"]";
+    public String toString() {
+        return "Zwierze: [id=" + _id + ", gatunek=" + gatunek + ", kolor= " + kolor + ", wielkosc = " + wielkosc + "]";
     }
 
     //Metoda zwracajaca opis zwierzęcia
-    public String getOpis(){return opis;}
-    public String getGatunek(){return gatunek;}
-    public String getKolor(){return kolor;}
-    public float getWielkosc(){return wielkosc;}
-    public int getId(){return _id;}
-    public void setId(int id){this._id=id;}
+    public String getOpis() {
+        return opis;
+    }
+
+    public String getGatunek() {
+        return gatunek;
+    }
+
+    public String getKolor() {
+        return kolor;
+    }
+
+    public float getWielkosc() {
+        return wielkosc;
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int id) {
+        this._id = id;
+    }
 
 }
--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,20 +19,54 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> target;
-    private ArrayAdapter adapter;
+    private SimpleCursorAdapter adapter;
+    MySQLite db = new MySQLite(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] values = new String[] { "Pies", "Kot", "Koń","Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+        String[] values = new String[]{"Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch"};
 
         this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
 
-        this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
 
-        ListView listview = (ListView) findViewById( R.id.listView );listview.setAdapter(this.adapter);
+        //this.adapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_1,this.target);
+        this.adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                db.lista(),
+                new String[]{"_id", "gatunek"},
+                new int[]{android.R.id.text1,
+                        android.R.id.text2},
+
+                SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
+        ListView listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(this.adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
+                Intent intencja = new Intent(getApplicationContext(), DodajWpis.class);
+                intencja.putExtra("element", zwierz);
+                startActivityForResult(intencja, 2);
+            }
+        });
+
+//        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//                TextView name = (TextView) view.findViewById(android.R.id.text1);
+//                Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
+//                db.usun(zwierz);
+//                adapter.changeCursor(db.lista());
+//                adapter.notifyDataSetChanged();
+//                return true;
+//            }
+//        });
     }
 
     @Override
@@ -44,13 +82,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if(requestCode==1 && resultCode==RESULT_OK)
-        {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            String nowy = (String)extras.get("wpis");
-            target.add(nowy);adapter.notifyDataSetChanged();
+            //String nowy = (String)extras.get("wpis");
+            Animal nowy = (Animal)
+                    extras.getSerializable("nowy");
+            this.db.dodaj(nowy);
+            //target.add(nowy);
+            adapter.changeCursor(db.lista());
+            adapter.notifyDataSetChanged();
+        }
+        if (requestCode == 2 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            //String nowy = (String)extras.get("wpis");
+            Animal nowy = (Animal)
+                    extras.getSerializable("nowy");
+            this.db.aktualizuj(nowy);
+            //target.add(nowy);
+            adapter.changeCursor(db.lista());
+            adapter.notifyDataSetChanged();
         }
     }
 }
